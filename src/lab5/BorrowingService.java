7748ee6abc168b2cb3ccdf90c80e676914b7a4e5
Nick <dragonlordbest666@gmail.com>
2024-11-28 14:25:41 -0800

    BookAndBorrowBook

--- src/lab5/BorrowingService.java
+++ src/lab5/BorrowingService.java
@@ -3,20 +3,31 @@ package lab5;
 public class BorrowingService implements BorrowingServiceAPI {
 	// TODO: Implement methods for borrowing 
 	// and returning Books 
-	
+
 	@Override 
 	public boolean borrowBook(Member member, Book book) { 
-	// Here you can implement logic to check if the book is available to 
-	// borrow and if the member can borrow it  
-	//(e.g., item limit, member status).  
-	System.out.println("Borrowing book: " + book); 
-	return true; // Return true for success 
+		// Here you can implement logic to check if the book is available to 
+		// borrow and if the member can borrow it  
+		//(e.g., item limit, member status).  
+		if(book.getIsAvailable()) {
+			member.borrowBook(book);
+			System.out.println("Borrowing book: " + book); 
+			return true; // Return true for success 
+		} 
+		System.out.println("Book is not available");
+		return false;
 	}
 
 	@Override
 	public boolean returnBook(Member member, Book book) {
-		// Implement logic to handle returning a book 
-		System.out.println("Returning book: " + book); 
-		return true; // Return true for success 
+		// Implement logic to handle returning a book
+		if(member.getBorrowedBooks().equals(book)) {
+			member.returnBook(book);
+			System.out.println("Returning book: " + book);
+			return true; // Return true for success 
+		}
+		
+		System.out.println("Member doesn't have that book");
+		return false;
 	}
 }
